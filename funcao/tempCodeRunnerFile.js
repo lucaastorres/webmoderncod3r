6de6f8ba3f43418d9d006c32719c419b777c447f
lucaastorres <lucaas.torres@live.com>
2020-09-16 15:06:37 -0300

    add arrowfunction/callback for function folder

---
+++ funcao/tempCodeRunnerFile.js
@@ -0,0 +1,13 @@
+const obj = {}
+// comparaComThis = comparaComThis.bind(obj)
+// comparaComThis(global)
+// comparaComThis(obj)
+
+// let comparaComThisArrow = param = console.log(this === param)
+// comparaComThisArrow(global)
+// comparaComThisArrow(this)
+// comparaComThisArrow(module.exports)
+
+// comparaComThisArrow = comparaComThisArrow.bind(obj)
+// comparaComThisArrow(obj)
+// comparaComThisArrow(module.exports)
