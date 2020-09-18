02170d0dba51aac82446f4b217f9f58de6d33173
lucaastorres <lucaas.torres@live.com>
2020-09-18 19:55:54 -0300

    forEach

---
+++ array/forEach1.js
@@ -0,0 +1,11 @@
+const aprovados =  ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach(function(nome, indice, array, x) {
+    console.log(`${indice + 1}) ${nome}`)
+    console.log(x)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
