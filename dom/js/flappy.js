962ea8a7baa095928cea4829afb63a25d80d7a03
lucaastorres <lucaas.torres@live.com>
2020-11-09 19:00:05 -0300

    flappy 5

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -100,6 +100,14 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo / 2)
 }
 
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso')
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
+
 // const barreiras = new Barreiras(700, 1200, 400, 300)
 // const passaro = new Passaro(700)
 // const areaDoJogo = document.querySelector('[wm-flappy]')
@@ -110,3 +118,31 @@ function Passaro(alturaJogo) {
 //     barreiras.animar()
 //     passaro.animar()
 // }, 20)
+
+function FlappyBird() {
+    let pontos = 0
+
+    const areaDoJogo =document.querySelector('[wm-flappy]')
+    const altura = areaDoJogo.clientHeight
+    const largura = areaDoJogo.clientWidth
+
+    const progresso = new Progresso()
+    const barreiras = new Barreiras(altura, largura, 200, 300, 
+        () => progresso.atualizarPontos(++pontos))
+    const passaro = new Passaro(altura)
+
+    areaDoJogo.appendChild(progresso.elemento)
+    areaDoJogo.appendChild(passaro.elemento)
+    barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+    
+
+    this.start = () => {
+        // loop do jogo
+        const temporizador = setInterval(() => {
+            barreiras.animar()
+            passaro.animar()
+        }, 20)
+    }
+}
+
+new FlappyBird().start()
